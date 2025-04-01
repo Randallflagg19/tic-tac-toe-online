31065a7a5aad7afe249371b89ff13297399dfc3e
Randallflagg19 <121073882+Randallflagg19@users.noreply.github.com>
2025-04-01 19:33:03 +0300

    new start

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import {Button} from '@/shared/ui/button'
+
+export default function Home() {
+  return (
+    <div>
+      <Button className='mt-10 ml-20' size='lg'>hellow</Button>
+    </div>
+  );
+}
