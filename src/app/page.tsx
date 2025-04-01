63494ecffb6a756b34c61a9eb75e395cc12dd627
Randallflagg19 <121073882+Randallflagg19@users.noreply.github.com>
2025-04-01 22:59:07 +0300

    added prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import {Button} from '@/shared/ui/button'
 export default function Home() {
   return (
     <div>
-      <Button className='mt-10 ml-20' size='lg'>hellow</Button>
+      <Button variant={'destructive'} className='mt-10 ml-20' size='lg'>hellow</Button>
     </div>
   );
 }
