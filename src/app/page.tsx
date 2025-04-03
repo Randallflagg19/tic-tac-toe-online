21abba14a1fc46b1981262247be0de90b2993a69
Randallflagg19 <121073882+Randallflagg19@users.noreply.github.com>
2025-04-03 14:34:22 +0300

    wo bd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col gap-4 container mx-auto pt-[100px]">
       <h1 className='text-4xl font-bold'>Игры</h1>
-      <GamesList/>
+      {/*<GamesList/>*/}
     </div>
   );
 }
